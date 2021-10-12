2c2688dfb60249efa9a034df7ab4b9cda1b22691
Yuki <76559774+Sleepwalker0416@users.noreply.github.com>
2021-10-12 22:14:52 +0700

    Update InitAssets.py

--- Source/InitAssets.py
+++ Source/InitAssets.py
@@ -56,35 +56,7 @@ class Font():
         for x in range(0):
             Screen.Resolution.screen.blit(Background.forest_img, ((x * width) - Var.GameVar.bg_scroll * 0.5, 0))            
 
-class Explosion(pygame.sprite.Sprite):
-	def __init__(self, x, y, scale):
-		pygame.sprite.Sprite.__init__(self)
-		self.images = []
-		for num in range(1, 6):
-			img = pygame.image.load(f'Assets/Image/Explosion/exp{num}.png').convert_alpha()
-			img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-			self.images.append(img)
-		self.frame_index = 0
-		self.image = self.images[self.frame_index]
-		self.rect = self.image.get_rect()
-		self.rect.center = (x, y)
-		self.counter = 0
-
-
-	def update(self):
-		EXPLOSION_SPEED = 4
-		#update explosion amimation
-		self.counter += 1
-
-		if self.counter >= EXPLOSION_SPEED:
-			self.counter = 0
-			self.frame_index += 1
-			#if the animation is complete then delete the explosion
-			if self.frame_index >= len(self.images):
-				self.kill()
-			else:
-				self.image = self.images[self.frame_index]
-
+	
 class SpriteGr():
 	enemy_group = pygame.sprite.Group()
 	bullet_group = pygame.sprite.Group()
